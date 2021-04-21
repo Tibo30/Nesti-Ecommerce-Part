a761bf36c9df54c6cb1decdf2f7ade1d4a7d7c0f
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-21 20:11:49 +0200

    empty entity

--- app/Entities/Tag.php
+++ app/Entities/Tag.php
@@ -5,9 +5,6 @@ namespace App\Entities;
 use CodeIgniter\Entity;
 
 class Tag extends Entity {     
-        protected $attributes = [
-            'id_tag' => null,
-            'name' => null,        // Represents a username
-        ];
+        
 }
 
