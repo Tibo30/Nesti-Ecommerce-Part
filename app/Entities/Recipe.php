69d0b2f3466b0b69ba910ec903da95356b9715bb
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-21 20:11:53 +0200

    empty entity

--- app/Entities/Recipe.php
+++ app/Entities/Recipe.php
@@ -5,18 +5,6 @@ use CodeIgniter\Entity;
 
 class Recipe extends Entity
 {
-    protected $attributes = [
-        'id_recipes' => null,
-        'creation_date' => null,        
-        'recipe_name' => null,
-        'difficulty' => null,
-        'number_of_people' => null,
-        'state' => null,
-        'time' => null,
-        'id_pictures' => null,
-        'id_chief' => null,
-        'paragraphs' => null,
-        'ingredients' => null,
-    ];
+
    
 }
