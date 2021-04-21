bf9899677e44137fdb0cf155099dbfb2a904dccf
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-21 20:11:46 +0200

    empty entity

--- app/Entities/Picture.php
+++ app/Entities/Picture.php
@@ -5,11 +5,6 @@ namespace App\Entities;
 use CodeIgniter\Entity;
 
 class Picture extends Entity {     
-        protected $attributes = [
-            'id_pictures' => null,
-            'creation_date' => null,
-            'name' => null,        
-            'extension' => null,
-        ];
+       
 }
 
