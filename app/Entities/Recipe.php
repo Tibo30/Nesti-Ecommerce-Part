2c59514639bf40e39e8fc480c4d4b9ada39be9be
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-23 10:03:45 +0200

    add getTags and getPicture methods

--- app/Entities/Recipe.php
+++ app/Entities/Recipe.php
@@ -1,10 +1,24 @@
 <?php
 namespace App\Entities;
 use CodeIgniter\Entity;
+use App\Models\TagModel;
+use App\Models\PictureModel;
 
 
 class Recipe extends Entity
 {
 
+    public function getTags(){
+        $tagModel = new TagModel();
+        $tags = $tagModel -> tagsByRecipe($this->id_recipes);
+        return $tags;
+    }
+
+    public function getPicture(){
+
+        $pictureModel = new PictureModel();
+        $picture = $pictureModel -> find($this->id_pictures);
+        return $picture;
+    }
    
 }
