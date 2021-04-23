475353dd8cfe3b626e744a6834acc45fc016e8bc
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-23 10:04:34 +0200

    add getOneRecipe method

--- app/Entities/Tag.php
+++ app/Entities/Tag.php
@@ -3,8 +3,19 @@
 namespace App\Entities;
 
 use CodeIgniter\Entity;
+use App\Models\RecipeModel;
+use App\Entities\Recipe;
 
-class Tag extends Entity {     
-        
+class Tag extends Entity
+{
+    // private Recipe $recipe;
+
+    public function getOneRecipe()
+    {
+        $recipeModel = new RecipeModel();
+        $recipes = $recipeModel->getRecipesbyTag($this->name);
+        shuffle($recipes);
+        $recipe = new Recipe(get_object_vars($recipes[0]));
+        return $recipe;
+    }
 }
-
