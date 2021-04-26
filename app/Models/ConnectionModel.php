a292b670efe5e2393128922f9f0c6cbd039c88d0
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-26 20:18:09 +0200

    create connection model

---
+++ app/Models/ConnectionModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use App\Entities\User;
+
+class ConnectionModel extends Model
+{
+
+    protected $table = 'users'; // nom de la table
+    protected $primaryKey = 'id_users';
+    protected $allowedFields = ['id_users', 'lastname', 'firstname', 'username', 'email', 'password', 'state', 'creation_date', 'address1', 'address2', 'poscode', 'id_city']; // Nom des colonnes autorisées
+    protected $returnType = User::class; // Type de retour
+    protected $useTimestamps = false; // Utilisation du timestamps
+    protected $skipValidation  = true;
+
+    public function checkUser($emailUsername)
+    {
+        $builder = $this->db->table('users');
+        $builder->where("email", $emailUsername);
+        $builder->orwhere("username", $emailUsername);
+        $query = $builder->get();
+
+        return $query->getResult();
+    }
+}
