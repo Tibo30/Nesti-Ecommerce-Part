483beec59258f4a937d85be87424bfd056ae77ca
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-27 19:56:51 +0200

    create entity

---
+++ app/Entities/Ingredient.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Entities;
+use CodeIgniter\Entity;
+use App\Models\PictureModel;
+use App\Models\UnitMeasureModel;
+use App\Models\IngredientModel;
+
+class Ingredient extends Entity
+{
+
+
+}
