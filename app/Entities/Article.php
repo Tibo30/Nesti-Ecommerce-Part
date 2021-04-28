1fcffc8032d4bc86db60f7805613e825bd181a16
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-28 19:36:16 +0200

    add getstock

--- app/Entities/Article.php
+++ app/Entities/Article.php
@@ -4,6 +4,8 @@ use CodeIgniter\Entity;
 use App\Models\PictureModel;
 use App\Models\UnitMeasureModel;
 use App\Models\IngredientModel;
+use App\Models\LotModel;
+use App\Models\OrderLineModel;
 
 class Article extends Entity
 {
@@ -25,6 +27,30 @@ class Article extends Entity
         $ingModel = new IngredientModel();
         $ing = $ingModel -> find($this->id_products);
         return $ing;
+    }
+
+    public function getStock()
+    {
+        $lotModel = new LotModel();
+        $orderLineModel = new OrderLineModel();
+        $orderLines = $orderLineModel->where("id_article",$this->id_article)->findAll(); // get all the order lines for an article
+        $lots = $lotModel->where("id_article",$this->id_article)->findAll(); // get all the lots for an article
+        $quantityOrdered = 0;
+        $quantityBought=0;
+        foreach ($orderLines as $orderLine) {
+            $quantityOrdered += $orderLine->quantity_ordered;
+        }
+        foreach($lots as $lot){
+            $quantityBought += $lot->bought_quantity;
+        }
+        $stock=$quantityBought-$quantityOrdered;
+        return $stock;
+    }
+
+    public function getName(){
+        $name=$this->quantity_per_unit." ".$this->getUnitMeasure()->name." of ".$this->getIngredient()->product_name;
+
+        return $name;
     }
 
 }
