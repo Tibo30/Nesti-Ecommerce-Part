67b0576c1b5094495c96365542d2da3970204590
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-28 19:36:35 +0200

    correct error

--- app/Models/ArticleModel.php
+++ app/Models/ArticleModel.php
@@ -9,8 +9,8 @@ class ArticleModel extends Model
 {
 
     protected $table = 'articles'; // nom de la table
-    protected $primaryKey = 'id_articles';
-    protected $allowedFields = ['id_articles','quantity_per_unit','state','creation_date','update_date','id_unit_measures','id_products','id_pictures','user_article_name']; // Nom des colonnes autorisées
+    protected $primaryKey = 'id_article';
+    protected $allowedFields = ['id_article','quantity_per_unit','state','creation_date','update_date','id_unit_measures','id_products','id_pictures','user_article_name']; // Nom des colonnes autorisées
     protected $returnType = Article::class; // Type de retour
     protected $useTimestamps = false; // Utilisation du timestamps
     protected $skipValidation  = true;
