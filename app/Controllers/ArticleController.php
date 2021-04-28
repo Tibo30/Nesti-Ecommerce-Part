0021a1dcb27880ea976daf58d74f1083dca0079f
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-28 19:35:53 +0200

    add article method in controller

--- app/Controllers/ArticleController.php
+++ app/Controllers/ArticleController.php
@@ -23,11 +23,50 @@ class ArticleController extends BaseController
     {
         helper('form');
         $model = new ArticleModel();
+        // $articles = $model->orderBy("id_products","asc")->findAll();
         $articles = $model->findAll();
+        usort($articles, function ($r1, $r2) { // sort the array ASC according to the product name of the article
+            return $r1->getIngredient()->product_name <=> $r2->getIngredient()->product_name;
+        });
 
         $data["loc"] = "Articles";
         $data["articles"] = $articles;
         return $this->twig->render("pages/articles.html", $data);
+    }
+
+    
+    /**
+     * Get the article information
+     */
+    public function article($idArticle)
+    {
+        helper('form');
+
+        $articleModel = new ArticleModel();
+        $recipeIngredientModel = new RecipeIngredientModel();
+        $recipeModel = new RecipeModel();
+
+        $article = $articleModel->find($idArticle); // get the article object
+        if ($article!=null){
+
+            $idIngredient = $article->getIngredient()->id_products; // get the id of the ingredient for this article
+
+            $recipesIng = $recipeIngredientModel->where("id_ingredients",$idIngredient)->findAll(); // get all the recipes ing obj for this ingredient
+    
+            $recipes=[];
+            foreach($recipesIng as $ing){ // get all the recipes for this ingredient
+                $recipes[]=$recipeModel->where("id_recipes",$ing->id_recipes)->find()[0];
+            }
+    
+            
+            $data["loc"] = "Article";
+            $data["article"] = $article;
+            $data["recipes"] = $recipes;
+            return $this->twig->render("pages/article.html", $data);
+        } else {
+            return redirect()->back();
+        }
+        
     }
 
   
