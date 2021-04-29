a88858428abeb74467930f892f6f2ef5cb285f99
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-04-29 20:05:03 +0200

    add get grades and get average grade

--- app/Entities/Recipe.php
+++ app/Entities/Recipe.php
@@ -1,23 +1,51 @@
 <?php
+
 namespace App\Entities;
+
 use CodeIgniter\Entity;
 use App\Models\TagModel;
 use App\Models\PictureModel;
+use App\Models\GradeModel;
 
 class Recipe extends Entity
 {
 
-    public function getTags(){
+    public function getTags()
+    {
         $tagModel = new TagModel();
-        $tags = $tagModel -> tagsByRecipe($this->id_recipes);
+        $tags = $tagModel->tagsByRecipe($this->id_recipes);
         return $tags;
     }
 
-    public function getPicture(){
+    public function getPicture()
+    {
 
         $pictureModel = new PictureModel();
-        $picture = $pictureModel -> find($this->id_pictures);
+        $picture = $pictureModel->find($this->id_pictures);
         return $picture;
     }
 
+    public function getGrades()
+    {
+        $gradeModel = new GradeModel();
+        $grades = $gradeModel->where('id_recipes', $this->id_recipes)->findAll(); // get all the grades for a recipe
+        return count($grades);
+    }
+
+    public function getAverageGrade()
+    {
+        $gradeModel = new GradeModel();
+        $grades = $gradeModel->where('id_recipes', $this->id_recipes)->findAll(); // get all the grades for a recipe
+
+        // calcul the average grade
+        $totalGrades = 0;
+        foreach ($grades as $grade) {
+            $totalGrades += $grade->grade;
+        }
+        $averageGrade = null;
+        if (count($grades) > 0) {
+            $averageGrade = $totalGrades / count($grades);
+        }
+        return $averageGrade;
+    }
 }
