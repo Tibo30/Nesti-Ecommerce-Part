58bbcc71f06834e46367947b4c9d52bbc9765adc
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-04 19:49:58 +0200

    correct error postcode

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
 
     protected $table = 'users'; // nom de la table
     protected $primaryKey = 'id_users';
-    protected $allowedFields = ['id_users', 'lastname', 'firstname', 'username', 'email', 'password', 'state', 'creation_date', 'address1', 'address2', 'poscode', 'id_city']; // Nom des colonnes autorisées
+    protected $allowedFields = ['id_users', 'lastname', 'firstname', 'username', 'email', 'password', 'state', 'creation_date', 'address1', 'address2', 'postcode', 'id_city']; // Nom des colonnes autorisées
     protected $returnType = User::class; // Type de retour
     protected $useTimestamps = false; // Utilisation du timestamps
     protected $skipValidation     = true;
