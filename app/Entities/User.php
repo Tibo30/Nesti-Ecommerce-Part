8341863749483312c902e8d0aedc02a6687fbf83
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-06 20:28:48 +0200

    add get city to user

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,16 +1,26 @@
 <?php
+
 namespace App\Entities;
+
 use CodeIgniter\Entity;
 use App\Models\GradeModel;
+use CodeIgniter\City;
+use App\Models\CityModel;
 
 class User extends Entity
 {
 
-    public function getGrade($idRecipe){
+    public function getGrade($idRecipe)
+    {
         $gradeModel = new GradeModel();
-        $grade = $gradeModel -> where('id_users',$this->id_users)->where('id_recipes',$idRecipe)->find();
+        $grade = $gradeModel->where('id_users', $this->id_users)->where('id_recipes', $idRecipe)->find();
         return $grade;
-    }    
+    }
 
-
+    public function getCity()
+    {
+        $cityModel = new CityModel();
+        $city = $cityModel->find($this->id_city);
+        return $city;
+    }
 }
