0cacf9fb96d152be4a71fc3911996a368e924a0d
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-06 20:28:08 +0200

    update

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+use App\Entities\User;
+
+
+
 class CartController extends BaseController
 {
     // protected $cart;
@@ -10,7 +15,7 @@ class CartController extends BaseController
     //  */
     // public function postCart()
     // {
-       
+
     //     // refresh CSRF token
     //     $data['csrf_token'] = csrf_hash();
     //     $this->cart= $this->request->getPost('cart'); // get the cart from the post
@@ -21,7 +26,8 @@ class CartController extends BaseController
     //     die;
     // }
 
-    public function cart(){
+    public function cart()
+    {
 
         //var_dump($this->cart);
         $data["loc"] = "Cart";
@@ -29,12 +35,17 @@ class CartController extends BaseController
         return $this->twig->render("pages/cart.html", $data);
     }
 
-    public function pay(){
+    public function pay()
+    {
+
+        $userModel = new UserModel();
+        $user = $userModel->find($_SESSION['id']);
 
         $data["loc"] = "Pay";
+        $data["user"] = $user;
         return $this->twig->render("pages/pay.html", $data);
     }
 
 
-  
+    
 }
