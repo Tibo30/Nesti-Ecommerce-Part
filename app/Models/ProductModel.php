41c24c4213780fda1ad59c7411a7c9715ef21c9a
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-07 13:52:42 +0200

    create

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use App\Entities\Product;
+
+class ProductModel extends Model
+{
+
+    protected $table = 'products'; // nom de la table
+    protected $primaryKey = 'id_products';
+    protected $allowedFields = ['id_products', 'product_name']; // Nom des colonnes autorisées
+    protected $returnType = Product::class; // Type de retour
+    protected $useTimestamps = false; // Utilisation du timestamps
+    protected $skipValidation  = true;
+
+    public function getIngredients($idRecipe)
+    {
+        $builder = $this->db->table('recipe_ingredients');
+        $builder->select('ingredients.id_ingredients, products.product_name, recipe_ingredients.quantity,unit_measures.name AS unit_name');
+        $builder->join('ingredients', 'ingredients.id_ingredients = recipe_ingredients.id_ingredients');
+        $builder->join('products', 'ingredients.id_ingredients = products.id_products');
+        $builder->join('unit_measures', 'unit_measures.id_unit_measures = recipe_ingredients.id_unit_measures');
+        $builder->where('recipe_ingredients.id_recipes', $idRecipe);
+        $query = $builder->get();
+
+        return $query->getResult();
+    }
+
+    
+    public function searchProduct(String $name){
+        $builder = $this->db->table('products');
+        $builder->like("product_name", "%".$name."%");
+        $query = $builder->get();
+        return $query->getResult();
+    }
+
+    public function isIngredient($idProduct){
+        $query = $this->db->query('SELECT * FROM ingredients WHERE id_ingredients="' . $idProduct . '"');
+        return $query->getResult();
+    }
+
+
+}
