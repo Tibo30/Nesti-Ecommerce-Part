34cad407142efb9da85aebc7e3fa97d92ceb43fe
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-05-24 16:51:23 +0200

    add connected condition to pay

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -9,41 +9,33 @@ use App\Entities\User;
 
 class CartController extends BaseController
 {
-    // protected $cart;
-    // /**
-    //  * 
-    //  */
-    // public function postCart()
-    // {
-
-    //     // refresh CSRF token
-    //     $data['csrf_token'] = csrf_hash();
-    //     $this->cart= $this->request->getPost('cart'); // get the cart from the post
-    //     var_dump($this->cart);
-    //     $data['success'] = true;
-    //     header('Content-Type: application/json');
-    //     echo json_encode($data);
-    //     die;
-    // }
-
+    /**
+     * cart page
+     */
     public function cart()
     {
-
-        //var_dump($this->cart);
         $data["loc"] = "Cart";
-        //$data["cart"] = $this->cart;
         return $this->twig->render("pages/cart.html", $data);
     }
 
+    /**
+     * pay page
+     */
     public function pay()
     {
 
-        $userModel = new UserModel();
-        $user = $userModel->find($_SESSION['id']);
-
-        $data["loc"] = "Pay";
-        $data["user"] = $user;
-        return $this->twig->render("pages/pay.html", $data);
+        if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true){ // if the user is connected
+            $userModel = new UserModel();
+            $user = $userModel->find($_SESSION['id']);
+            $data["loc"] = "Pay";
+            $data["user"] = $user;
+            return $this->twig->render("pages/pay.html", $data);
+        } else { // else go to login
+            $data["base_dir"]=__DIR__;
+            $data["loc"]="Login";
+            return $this->twig->render("pages/login.html", $data);
+        }
+       
     }
 
 
