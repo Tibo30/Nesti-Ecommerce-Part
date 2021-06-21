df494346a759966698f8add3617e724f91630c09
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-06-21 16:56:46 +0200

    update regex name (mini 2 characters)

--- app/Libraries/MyRules.php
+++ app/Libraries/MyRules.php
@@ -2,17 +2,26 @@
 
 namespace App\Libraries;
 
+/**
+ * Rules defined for regex
+ */
 class MyRules
 {
+    /**
+     * This is the regex method to check name
+     */
     public function regex_check_name($str)
     {
-        if (!preg_match("/^[a-zA-ZÀ-ÿ ,.'-]{3,20}+$/i", $str)) {
+        if (!preg_match("/^[a-zA-ZÀ-ÿ ,.'-]{2,20}+$/i", $str)) {
             return FALSE;
         } else {
             return TRUE;
         }
     }
 
+    /**
+     * This is the regex method to check password
+     */
     public function regex_check_password($str)
     {
         if (!preg_match("/^(?=.*?[A-Z])(?=(.*[a-z]))(?=(.*[\d]))(?=(.*[\W]))(?!.*\s).{12,}$/", $str)) {
@@ -22,6 +31,9 @@ class MyRules
         }
     }
 
+    /**
+     * This is the regex method to check username
+     */
     public function regex_check_username($str)
     {
         if (!preg_match("/^[a-zA-ZÀ-ÿ0-9._-]{7,20}$/", $str)) {
@@ -31,6 +43,9 @@ class MyRules
         }
     }
 
+    /**
+     * This is the regex method to check the postcode
+     */
     public function regex_check_postcode($str)
     {
         if (!preg_match("/^[0-9]{5}$/", $str)) {
